e919880ffe41b2974faf4635d276dda6b1e83b77
Baltazar Ochagavia <bochagavia@MacBook-Pro-de-Baltazar.local>
2016-10-10 20:22:46 -0300

    Set default room: Santiago

--- chatapp/app.js
+++ chatapp/app.js
@@ -25,15 +25,15 @@ io.sockets.on('connection', function (socket) {
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = 'room1';
+		socket.room = 'Santiago';
 		// add the client's username to the global list
 		usernames[username] = username;
 		// send client to room 1
-		socket.join('room1');
+		socket.join('Santiago');
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+		socket.emit('updatechat', 'SERVER', 'you have connected to Santiago');
 		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.broadcast.to('Santiago').emit('updatechat', 'SERVER', username + ' has connected to this room');
 		socket.emit('updaterooms', rooms, 'room1');
 	});
 	
