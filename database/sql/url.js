6edc48668db6fa5a21e720dfbb2bce05e06ce76b
Antonio Fontaine <afontaine@uc.cl>
2016-10-13 23:46:16 -0300

    misstype

--- database/sql/url.js
+++ database/sql/url.js
@@ -1,4 +1,4 @@
 // config/database.js
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:127.0.0.1/geochat';
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres@127.0.0.1/geochat';
 
 module.exports = connectionString;
