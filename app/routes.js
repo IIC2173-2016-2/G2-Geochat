85399b2c0fd1ab3615987bb492bdcbd81890c3af
Antonio Fontaine <afontaine@uc.cl>
2016-10-29 13:52:36 -0300

    update done, need to make sure not to edit other users

--- app/routes.js
+++ app/routes.js
@@ -83,6 +83,7 @@ module.exports = function router(app, passport) {
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
+  console.log(req);
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated()) {
     return next();
