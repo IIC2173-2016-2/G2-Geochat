d82e8e8c776195fc05d82d2567af4be72e9fe637
Antonio Fontaine <afontaine@uc.cl>
2016-11-03 10:42:15 -0300

    list places as json in chatapp

--- chatapp/app/routes.js
+++ chatapp/app/routes.js
@@ -1,11 +1,18 @@
 // app/routes.js
 
+const Place = require('./mongoModels/place');
+
 
 module.exports = function router(app) {
   // =====================================
   // HOME PAGE (with login links) ========
   // =====================================
   app.get('/', (req, res) => {
-    res.render('index.ejs'); // load the index.ejs file
+    Place.find({}, (err, places) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.send(places);
+    });
   });
 };
